8f5207aeed106db4832ab7fc1ed6f1588edf55a7
macandos <87715564+macandos@users.noreply.github.com>
2022-06-01 22:23:57 +0100

    changed colours

--- src/colour.h
+++ src/colour.h
@@ -8,7 +8,8 @@
 #define DARK_GREY 235
 #define DARKER_GREY 234
 #define LIGHT_GREY 237
-#define LIGHTER_GREY 255
+#define LIGHTER_GREY 245
+#define WHITE 255
 
 void processBG(App* a, int colour);
 void processFG(App* a, int colour);
