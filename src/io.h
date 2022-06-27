65a7128c648d9c809cd575de3fbc7133928e4b10
macandos <87715564+macandos@users.noreply.github.com>
2022-06-27 20:15:21 +0100

    tab functionality

--- src/io.h
+++ src/io.h
@@ -7,6 +7,6 @@ void openFile(char* filename, DisplayInit *dinit);
 void createRow(char* text, size_t length, int pos, DisplayInit* dinit);
 void changeStr(char* str, size_t length, int pos, DisplayInit* dinit);
 int handleUnwantedChars(const char* text, ssize_t length);
-void writeFile(DisplayInit* dinit, App* a);
+void writeFile(DisplayInit* dinit);
 
 #endif
