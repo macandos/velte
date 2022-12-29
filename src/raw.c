47c3d9c1a65b33db2649b03b29e2ffb5ffc29cf1
macandos <87715564+macandos@users.noreply.github.com>
2022-12-29 12:43:57 +0000

    fixed flags

--- src/raw.c
+++ src/raw.c
@@ -11,30 +11,26 @@ struct termios term;
 // if exit, restore the terminal's original settings
 void endRaw() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term) == -1) 
-        errorHandle("tcsetattr");
-
-    // reset position
-    //printf("\033[0;0H");
-    printf("\033c");
+        errorHandle("Velte: tcsetattr");
+    //printf("\033c");
 }
 
 // used to read input one-at-a-time, instead of all at one line
 void raw() {
     // kill program when something fails
     if (tcgetattr(STDIN_FILENO, &term) == -1)
-        errorHandle("tcgetattr");
+        errorHandle("Velte: tcgetattr");
 
     atexit(endRaw);
-
     struct termios rawS = term;
 
     // disable a bunch of flags
-    rawS.c_lflag &= ~(ECHO | ISIG | IXON | ICANON | ICRNL | IEXTEN);
-    rawS.c_lflag &= ~(BRKINT | CS8 | INPCK | ISTRIP | IXOFF);
-
+    rawS.c_iflag &= ~(IXOFF | IXON | ICRNL | ISTRIP);
+    rawS.c_lflag &= ~(ECHO | ISIG | ICANON | IEXTEN);
+    
     rawS.c_cc[VMIN] = 0;
     rawS.c_cc[VTIME] = 1;
     
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawS) == -1) 
-        errorHandle("tcsetattr");
+        errorHandle("Velte: tcsetattr");
 }
